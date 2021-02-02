70593cfc31dcc7f77f2a1bbd659ae460b38ebb2f
V Veda <v.vedasbackup@gmail.com>
2021-02-02 08:33:25 +0530

    second platform

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup(){
     world = engine.world;
 
     platform = new Ground(150,305,300,170)    
+    platform2 = new Ground(150,15,300,170)  
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -61,4 +62,5 @@ function draw(){
     bird.display();
 
     platform.display()
+    platform2.display()
 }
